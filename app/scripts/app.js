d03010b0f9f3300fd35c500c4eee39fa3a7aaf21
jpokrzyk <jon@carematics.com>
2014-07-28 21:12:12 -0400

    initial

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name yoTestApp
+ * @description
+ * # yoTestApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('yoTestApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
